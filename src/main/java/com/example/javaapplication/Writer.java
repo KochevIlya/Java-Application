dc3a3f448935dc50e07e9f20a44cd6fe1102f45b
Ilya <kochev0987@gmail.com>
2023-12-03 15:00:03 +0300

    Writed Writer + Tests

--- src/main/java/com/example/javaapplication/Writer.java
+++ src/main/java/com/example/javaapplication/Writer.java
@@ -1,13 +1,101 @@
 package com.example.javaapplication;
 
+import java.io.FileWriter;
+import java.io.IOException;
+class WriterException extends Throwable {
+
+}
 public class Writer {
-    String fileName;
+    private String fileName;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+    public String writeToJson(Result result)
+    {
+        String str = "";
+        String tab = "    ";
+        str += "{\n";
+        for(int i = 0; i < result.getResultList().size(); i++)
+        {
+            str += tab +"\"example\" : " + "\"" + result.getSampleList().get(i) + "\"," + "\n";
+            str += tab + "\"result\" : " + "\"" + result.getResultList().get(i) + "\"," + "\n";
+        }
+        str +=tab + "\"allText\" : ";
+        str += "\"" +result.getInputText() + "\"" + "\n";
+        str += "}";
+        return str;
+    }
+    public String writeToXml(Result result)
+    {
+        String str = "";
+        String tab = "    ";
+        str += "<root>\n";
+        for(int i = 0; i < result.getResultList().size(); i++)
+        {
+            str += tab +"<example>" + result.getSampleList().get(i) + "</example>" + "\n";
+
+            str += tab + tab + "<result>" +  result.getResultList().get(i) + "</result>" + "\n";
+        }
+        str += tab + "<allText>";
+        str += result.getInputText();
+        str += "</allText>\n";
+        str += "</root>";
+        return str;
+    }
+
+
     public Writer(String fileName)
     {
-        fileName = this.fileName;
+        setFileName(fileName);
     }
-    public void write(Result result)
-    {
-           return;
+    public void write(Result result)  {
+        try {
+            FileWriter fileWriter = new FileWriter(fileName);
+
+            int decision = result.getDecision();
+            if(result.getDecision() <1 || result.getDecision() > 5)
+            {
+                throw new WriterException();
+            }
+            switch(decision)
+            {
+                case 4:
+                    if(result.getSampleList() == null || result.getResultList() == null || result.getInputText() == null) {
+                        throw new WriterException();
+                    }
+                    fileWriter.write(writeToJson(result));
+                    break;
+                case 5:
+                    if(result.getSampleList() == null || result.getResultList() == null || result.getInputText() == null) {
+                        throw new WriterException();
+                    }
+                    fileWriter.write(writeToXml(result));
+                    break;
+
+                default:
+                    if(result.getReplacedText() == null)
+                    {
+                        throw new WriterException();
+                    }
+                    fileWriter.write(result.getReplacedText().trim());
+                    break;
+            }
+            fileWriter.close();
+        }
+        catch(IOException e)
+        {
+            System.out.println("File name is wrong");
+            System.exit(0);
+        }
+        catch(WriterException e )
+        {
+            System.out.println("Provided result is not full for that task");
+            System.exit(0);
+        }
     }
 }
--- src/test/java/com/example/javaapplication/WriterTest.java
+++ src/test/java/com/example/javaapplication/WriterTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,8 +22,124 @@ class WriterTest {
         return content.toString();
     }
     @Test
-    public void testWriteToTxt()
-    {
-
+    public void testWriteToXml() throws IOException {
+        Writer writer = new Writer("output.xml");
+        Result result = new Result();
+        result.setDecision(1);
+        String text = ("""
+                <hello> Something</hello>
+                <nothing> new </nothing>""");
+        result.setReplacedText(text);
+        writer.write(result);
+        String expectedText = text.replace("\n", System.lineSeparator());
+        assertEquals(expectedText, readContentFromFile("output.xml").trim());
+    }
+    @Test
+    public void testWriteToTxt() throws IOException {
+        Writer writer = new Writer("output.txt");
+        Result result = new Result();
+        result.setDecision(1);
+        String text = ("It it just simple text\n" +
+                "without any difficulties");
+        result.setReplacedText(text);
+        writer.write(result);
+        String expectedText = text.replace("\n", System.lineSeparator());
+        assertEquals(expectedText, readContentFromFile("output.txt").trim());
+    }
+    @Test
+    public void testWriteToJson() throws IOException {
+        Writer writer = new Writer("output.json");
+        Result result = new Result();
+        result.setDecision(1);
+        String text = """
+                {
+                "text" :  "Just simple text in Json"
+                }""";
+        result.setReplacedText(text);
+        writer.write(result);
+        String expectedText = text.replace("\n", System.lineSeparator());
+        assertEquals(expectedText, readContentFromFile("output.json").trim());
+    }
+    @Test
+    public void testWriteFromXmlToTxt() throws IOException {
+        Writer writer = new Writer("outputFromXml.txt");
+        Result result = new Result();
+        result.setDecision(1);
+        String text = ("<hello> Something</hello>\n" +
+                "<nothing> new </nothing>");
+        result.setReplacedText(text);
+        writer.write(result);
+        String expectedText = text.replace("\n", System.lineSeparator());
+        assertEquals(expectedText, readContentFromFile("outputFromXml.txt").trim());
+    }
+    @Test
+    public void testWriteFromJsonToTxt() throws IOException {
+        Writer writer = new Writer("outputFromJson.txt");
+        Result result = new Result();
+        result.setDecision(1);
+        String text = """
+                {
+                "text" :  "Just simple text in Json"
+                }""";
+        result.setReplacedText(text);
+        writer.write(result);
+        String expectedText = text.replace("\n", System.lineSeparator());
+        assertEquals(expectedText, readContentFromFile("outputFromJson.txt").trim());
+    }
+    @Test
+    public void testWriteFromTxtToXml() throws IOException {
+        Writer writer = new Writer("outputFromTxt.xml");
+        Result result = new Result();
+        result.setDecision(5);
+        String text = ("Hello it is just a simple text with samples 4 + 5, 4 - 3");
+        ArrayList<String> sampleList = new ArrayList<>();
+        sampleList.add("4 + 5");
+        sampleList.add("4 - 3");
+        ArrayList<Double> resultList = new ArrayList<>();
+        resultList.add(9.0);
+        resultList.add(1.0);
+        result.setSampleList(sampleList);
+        result.setResultList(resultList);
+        result.setInputText(text);
+        String expectedText = ("""
+                <root>
+                    <example>4 + 5</example>
+                        <result>9.0</result>
+                    <example>4 - 3</example>
+                        <result>1.0</result>
+                    <allText>Hello it is just a simple text with samples 4 + 5, 4 - 3</allText>
+                </root>""");
+        result.setReplacedText(text);
+        writer.write(result);
+        expectedText = expectedText.replace("\n", System.lineSeparator());
+        assertEquals(expectedText, readContentFromFile("outputFromTxt.xml").trim());
+    }
+    @Test
+    public void testWriteFromTxtToJson() throws IOException {
+        Writer writer = new Writer("outputFromTxt.json");
+        Result result = new Result();
+        result.setDecision(4);
+        String text = ("Hello it is just a simple text with samples 4 + 5, 4 - 3");
+        ArrayList<String> sampleList = new ArrayList<>();
+        sampleList.add("4 + 5");
+        sampleList.add("4 - 3");
+        ArrayList<Double> resultList = new ArrayList<>();
+        resultList.add(9.0);
+        resultList.add(1.0);
+        result.setSampleList(sampleList);
+        result.setResultList(resultList);
+        result.setInputText(text);
+        String expectedText = ("""
+                {
+                    "example" : "4 + 5",
+                    "result" : "9.0",
+                    "example" : "4 - 3",
+                    "result" : "1.0",
+                    "allText" : "Hello it is just a simple text with samples 4 + 5, 4 - 3"
+                }""");
+        result.setReplacedText(text);
+        writer.write(result);
+        expectedText = expectedText.replace("\n", System.lineSeparator());
+        assertEquals(expectedText, readContentFromFile("outputFromTxt.json").trim());
     }
 }
