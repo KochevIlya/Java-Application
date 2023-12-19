819fd6c45f3a82ad667616a364422a14b9ab9d37
Maria Astashkevich <tatciana2302@gmail.com>
2023-12-20 00:45:05 +0300

    Writed Finder

--- src/main/java/com/example/javaapplication/Finder.java
+++ src/main/java/com/example/javaapplication/Finder.java
@@ -1,6 +1,8 @@
 package com.example.javaapplication;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Finder {
@@ -13,7 +15,14 @@ public class Finder {
     }
     public void find(Result result)
     {
-        return;
+        String parseString = result.getInputText();
+        ArrayList<String> allMatches = new ArrayList<String>();
+        Pattern pattern = Pattern.compile("([()-+]*?[0-9]*\\.?[0-9]+[\\/\\+\\-\\*\\^\\(\\)]*)+([-+]?[0-9]*\\.?[0-9]*[()]*?)");
+        Matcher matcher = pattern.matcher(parseString);
+        while(matcher.find()) {
+            allMatches.add(matcher.group());
+        }
+        result.setSampleList(allMatches);
     }
 
 
