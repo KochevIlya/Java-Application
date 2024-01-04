32d54253e534ed65f7a7e7dcae5c22430a129328
Maria Astashkevich <tatciana2302@gmail.com>
2024-01-04 15:56:48 +0300

    Used Lombok Annotations + Fixed Finder

--- src/main/java/com/example/javaapplication/Result.java
+++ src/main/java/com/example/javaapplication/Result.java
@@ -4,19 +4,39 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
-@Setter
-@Getter
 public class Result {
+    @Getter
+    @Setter
     private ArrayList<Double> resultList = new ArrayList<>();
+    @Getter
+    @Setter
     private ArrayList<String> sampleList = new ArrayList<>();
+    @Getter
+    @Setter
     private ArrayList<Duration> positionsList = new ArrayList<>();
+    @Getter
+    @Setter
     private String inputText;
+    @Getter
+    @Setter
     private String replacedText;
+    @Getter
+    @Setter
     private ArrayList<MathExp> jsonNodes = new ArrayList<>();
+    @Getter
+    @Setter
     private ArrayList<HelperExpression> mathExpressions = new ArrayList<>();
+    @Getter
+    @Setter
     private int decision;
+    @Getter
+    @Setter
     private boolean shouldEncrypt;
+    @Getter
+    @Setter
     private boolean shouldArchive;
+    @Getter
+    @Setter
     private String encryptedKey;
 
     private boolean isFirstEncrypt;
@@ -25,90 +45,52 @@ public class Result {
         return isFirstEncrypt;
     }
 
-    public void setFirstEncrypt(boolean firstEncrypt) {
-        isFirstEncrypt = firstEncrypt;
-    }
+//    public void setFirstEncrypt(boolean firstEncrypt) {
+//        isFirstEncrypt = firstEncrypt;
+//    }
+//
+//    public void setShouldArchive(boolean shouldArchive) {
+//        this.shouldArchive = shouldArchive;
+//    }
+//
+//
+//    public void setEncryptedKey(String encryptedKey) {
+//        this.encryptedKey = encryptedKey;
+//    }
+//
+//
+//    public void setShouldEncrypt(boolean shouldEncrypt) {
+//        this.shouldEncrypt = shouldEncrypt;
+//    }
 
-    public boolean isShouldArchive() {
-        return shouldArchive;
-    }
+//    public void setResultList(ArrayList<Double> resultList) {
+//        this.resultList = resultList;
+//    }
 
-    public void setShouldArchive(boolean shouldArchive) {
-        this.shouldArchive = shouldArchive;
-    }
-
-
-
-    public String getEncryptedKey() {
-        return encryptedKey;
-    }
-
-    public void setEncryptedKey(String encryptedKey) {
-        this.encryptedKey = encryptedKey;
-    }
-
-
-    public boolean isShouldEncrypt() {
-        return shouldEncrypt;
-    }
-
-    public void setShouldEncrypt(boolean shouldEncrypt) {
-        this.shouldEncrypt = shouldEncrypt;
-    }
-
-    public ArrayList<Double> getResultList() {
-        return resultList;
-    }
-
-    public void setResultList(ArrayList<Double> resultList) {
-        this.resultList = resultList;
-    }
-
-    public ArrayList<String> getSampleList() {
-        return sampleList;
-    }
-
-    public void setSampleList(ArrayList<String> sampleList) {
-        this.sampleList = sampleList;
-    }
-
-    public ArrayList<Duration> getPositionsList() {
-        return positionsList;
-    }
-
-    public void setPositionsList(ArrayList<Duration> positionsList) {
-        this.positionsList = positionsList;
-    }
-
-    public String getInputText() {
-        return inputText;
-    }
-
-    public void setInputText(String inputText) {
-        this.inputText = inputText;
-    }
-
-    public String getReplacedText() {
-        return replacedText;
-    }
-
-    public void setReplacedText(String replacedText) {
-        this.replacedText = replacedText;
-    }
-
-    public int getDecision() {
-        return decision;
-    }
-
-    public void setDecision(int decision) {
-        this.decision = decision;
-    }
+//    public void setSampleList(ArrayList<String> sampleList) {
+//        this.sampleList = sampleList;
+//    }
+//
+//    public void setPositionsList(ArrayList<Duration> positionsList) {
+//        this.positionsList = positionsList;
+//    }
+//
+//    public void setInputText(String inputText) {
+//        this.inputText = inputText;
+//    }
+//
+//    public void setReplacedText(String replacedText) {
+//        this.replacedText = replacedText;
+//    }
+//
+//    public void setDecision(int decision) {
+//        this.decision = decision;
+//    }
     public Double getResultByIndex (int index) {return resultList.get(index); }
 
     public Duration getDurationByIndex (int index) {return positionsList.get(index); }
-    public ArrayList<MathExp> getJsonNodes() {return jsonNodes;}
 
-    public void setJsonNodes(ArrayList<MathExp> jsonNodes) {this.jsonNodes = jsonNodes;}
-    public ArrayList<HelperExpression> getMathExpressions() {return mathExpressions;}
-    public void setMathExpressions(ArrayList<HelperExpression> helperExpressions) {this.mathExpressions = helperExpressions;}
+//    public void setJsonNodes(ArrayList<MathExp> jsonNodes) {this.jsonNodes = jsonNodes;}
+//
+//    public void setMathExpressions(ArrayList<HelperExpression> helperExpressions) {this.mathExpressions = helperExpressions;}
 }
